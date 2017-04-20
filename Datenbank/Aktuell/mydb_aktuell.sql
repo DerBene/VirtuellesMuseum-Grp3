43d5ea4339c53c867f1190ad7b22741f4ddada12
smlhome <Steffen.Mg@gmail.com>
2017-04-20 15:44:03 +0200

    Aktuelle Datenstruktur

--- Datenbank/Aktuell/mydb_aktuell.sql
+++ Datenbank/Aktuell/mydb_aktuell.sql
@@ -1,3 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 20. Apr 2017 um 15:17
+-- Server-Version: 10.1.21-MariaDB
+-- PHP-Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Datenbank: `mydb`
 --
@@ -153,6 +171,10 @@ CREATE TABLE `werke_has_persoenlichkeiten` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
+-- Indizes der exportierten Tabellen
+--
+
+--
 -- Indizes für die Tabelle `epochen`
 --
 ALTER TABLE `epochen`
@@ -223,6 +245,10 @@ ALTER TABLE `werke_has_persoenlichkeiten`
   ADD PRIMARY KEY (`Werke_Werk_ID`,`Persoenlichkeiten_Pers_ID`),
   ADD KEY `fk_Werke_has_Persönlichkeiten_Persönlichkeiten1_idx` (`Persoenlichkeiten_Pers_ID`),
   ADD KEY `fk_Werke_has_Persönlichkeiten_Werke1_idx` (`Werke_Werk_ID`);
+																
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
 
 --
 -- AUTO_INCREMENT für Tabelle `epochen`
@@ -256,6 +282,9 @@ ALTER TABLE `users`
 --
 ALTER TABLE `werke`
   MODIFY `Werk_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints der exportierten Tabellen
+--
 
 --
 -- Constraints der Tabelle `epochen_has_kategorien`
@@ -296,3 +325,7 @@ ALTER TABLE `werke`
 ALTER TABLE `werke_has_persoenlichkeiten`
   ADD CONSTRAINT `fk_Werke_has_Persönlichkeiten_Persönlichkeiten1` FOREIGN KEY (`Persoenlichkeiten_Pers_ID`) REFERENCES `persoenlichkeiten` (`Pers_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Werke_has_Persönlichkeiten_Werke1` FOREIGN KEY (`Werke_Werk_ID`) REFERENCES `werke` (`Werk_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
